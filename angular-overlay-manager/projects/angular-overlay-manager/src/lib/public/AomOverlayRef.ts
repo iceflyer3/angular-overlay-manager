cdf788a6d1b63b1e273862ab74347321265b9f27
Josh <iceflyer3@live.com>
2019-03-25 22:09:03 -0400

    Observables were giving us issues when we were trying to open a secondary overlay in the close event of another one. There was general wonkiness with the observables list of subscribers going on. We have traded out the Observable for a Promise, which, if we're being honest, not only looks nicer with a call to then() instead of subscribe(), but also probably fits our onClose() use case better. Given that promises operate on a one off basis and subsequently don't maintain a list of "observers" like an Observable / Subject / etc... does in rxjs this has fixed the issue and the opening of secondary overlays from the onClose event of a another overlay is now working as desired.

--- angular-overlay-manager/projects/angular-overlay-manager/src/lib/public/AomOverlayRef.ts
+++ angular-overlay-manager/projects/angular-overlay-manager/src/lib/public/AomOverlayRef.ts
@@ -12,7 +12,7 @@ import { OverlayManager } from "../internal/OverlayManager";
 export class AomOverlayRef{
     constructor(private overlayManager: OverlayManager) {}
 
-    public onClose(): Observable<any>
+    public onClose(): Promise<any>
     {
         // Just bubble the event from the overlay manager up one more level
         return this.overlayManager.onClose();
